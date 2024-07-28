914a102b9ca138d89ef0d590b80f68ee56427ed2
“Lili <lilyhuang114classb@gmail.com>
2024-07-27 20:53:13 -0400

    add Claims struct.

--- types.go
+++ types.go
@@ -1,7 +1,15 @@
 package main
 
+import "github.com/dgrijalva/jwt-go"
+
 // Credentials struct to hold username and password
 type Credentials struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+// Claims struct to hold the JWT claims
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
 }
